3558ad549042b331510c9a75a8114e4fe8e91263
Aayaam Gupta <aayaam.g@360NDCPC143.360logica.com>
2025-07-01 11:13:14 +0530

    fixed bugs

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'ToDoList.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    DATABASES = {
-        'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
+
 
 
 # Password validation
